8aba458174f08e155ba8bc340842155f42fc8769
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2023-03-07 11:06:02 +0100

    Refactor Bridge.MDIO

--- src/Clash/Cores/Ethernet/Bridge/Mdio.hs
+++ src/Clash/Cores/Ethernet/Bridge/Mdio.hs
@@ -1,6 +1,7 @@
 module Clash.Cores.Ethernet.Bridge.Mdio (MDIOResponse (..), MDIORequest (..), mdioComponent) where
 
 import Clash.Prelude
+import Data.Maybe ( isNothing )
 
 data MDIORequest
   -- | PHY address, REG address
@@ -18,14 +19,8 @@ data MDIOResponse
 data PacketState
   = SendPreamble
   -- ^ Send 32 times a 1
-  | SendST
-  -- ^ Send 01
-  | SendOP
-  -- ^ Send 10 for read, 01 for write
-  | SendPA5
-  -- ^ Send the PHY address
-  | SendRA5
-  -- ^ Send the REG address
+  | SendSTOPPARA
+  -- ^ Send the STart, OPeration, Physical Address and Register Address
   | SendTAWrite
   -- ^ Send 10
   | SendTARead
@@ -38,115 +33,80 @@ data PacketState
   -- ^ Waiting for the bridge to issue a new request
   deriving (Show, Eq, Generic, NFDataX)
 
--- PacketState, counter, MDIORequest
-data State = State PacketState (Index 32) MDIORequest
+-- PacketState, counter, (cached STOPPARA, data to write)
+data State = State PacketState (Index 32) (BitVector 14, Maybe (BitVector 16))
   deriving (Show, Eq, Generic, NFDataX)
-
--- | Send the two bits in this order and then transition into a new `PacketState`
-sendTwo :: PacketState -> (Bit,Bit) -> State -> (State, (Maybe Bit, Maybe MDIOResponse))
-sendTwo nextStage (fstBit, sndBit) (State curState cnt req) = (nextState, (Just out, Nothing))
-  where
-    out
-      | cnt == 0 = fstBit
-      | otherwise = sndBit
-    nextState
-      | cnt == 0 = State curState 1 req
-      | otherwise = State nextStage 0 req
-
--- | Send an address and then transition into a new `PacketState`
-sendFive :: PacketState -> (Unsigned 5) -> State -> (State, (Maybe Bit, Maybe MDIOResponse))
-sendFive nextStage bv (State curState cnt req) = (nextState, (Just out, Nothing))
-  where
-    -- Get the `cnt`-th bit of the address
-    -- MSB first!
-    out = bv ! (5 - cnt)
-    nextState
-      | cnt < 4 = State curState (cnt+1) req
-      | otherwise = State nextStage 0 req
 
 -- | State transitions in the mealy machine
 nextStep :: State -> (Maybe MDIORequest, Bit) -> (State, (Maybe Bit, Maybe MDIOResponse))
 -- Nothing happens
 nextStep s@(State Idle _ _) (Nothing, _) = (s, (Nothing, Nothing))
 -- Receive a request from the bridge
-nextStep (State Idle _ _) (Just req, _)   = (State SendPreamble 0 req, (Nothing,Nothing))
+nextStep (State Idle _ _) (Just req, _)   = (nextState req, (Nothing,Nothing))
+    where
+        nextState (MDIORead phy reg) = State SendPreamble 0 (0b0110 ++# (pack phy) ++# (pack reg), Nothing)
+        nextState (MDIOWrite phy reg dat) = State SendPreamble 0 (0b0101 ++# (pack phy) ++# (pack reg), Just dat)
 
 -- Send Preamble
-nextStep (State SendPreamble cnt req) (_, _) = (nextState, (Just 1, Nothing))
+nextStep (State SendPreamble cnt d) (_, _) = (nextState, (Just 1, Nothing))
   where
     nextState
-      | cnt < 31 = State SendPreamble (cnt+1) req
-      | otherwise = State SendST 0 req
+      | cnt < 31 = State SendPreamble (cnt+1) d
+      | otherwise = State SendSTOPPARA 0 d
 
--- Send ST
-nextStep s@(State SendST _ _) (_, _) = sendTwo SendOP (0,1) s
-
--- Send OP
-nextStep s@(State SendOP _ req) (_, _) = sendTwo SendPA5 (op req) s
+-- Send all 14 bits that are the same for both read and write requests
+-- STart, OPeration, Physical Address, Register Address
+nextStep (State SendSTOPPARA cnt (sendBv, dat)) (_,_) = (nextState, (Just out, Nothing))
   where
-    -- Output 10
-    op (MDIORead _ _) = (1,0)
-    -- Output 01
-    op (MDIOWrite _ _ _) = (0,1)
-
--- Send PA5
-nextStep s@(State SendPA5 _ req) (_, _) = sendFive SendRA5 (phyAddr req) s
-  where
-    -- Get the PHY address
-    phyAddr :: MDIORequest -> (Unsigned 5)
-    phyAddr (MDIORead addr _) = addr
-    phyAddr (MDIOWrite addr _ _) = addr
-
--- Send RA5
-nextStep s@(State SendRA5 _ req) (_, _) = sendFive (nextStage req) (regAddr req) s
-  where
-    nextStage (MDIORead _ _) = SendTARead
-    nextStage (MDIOWrite _ _ _) = SendTAWrite
-    -- Get the REG address
-    regAddr :: MDIORequest -> (Unsigned 5)
-    regAddr (MDIORead _ addr) = addr
-    regAddr (MDIOWrite _ addr _) = addr
+    -- MSB first!
+    out = msb sendBv
+    bv' = shiftL sendBv 1
+    nextState
+      | cnt < 13 = State SendSTOPPARA (cnt+1) (bv', dat)
+      | isNothing dat = State SendTARead 0 (bv', dat)
+      | otherwise = State SendTAWrite 0 (bv', dat)
 
 -- Wait out the turn-around field for Read
-nextStep (State SendTARead cnt req) (_, _) = (nextState, (Nothing, Nothing))
+nextStep (State SendTARead cnt d) (_, _) = (nextState, (Nothing, Nothing))
   where
     -- TODO: Check that other side correctly sends 0 when cnt == 1?
     nextState
-      | cnt == 0 = State SendTARead 1 req
-      | otherwise = State (ReadD16 0) 0 req
+      -- cnt == 0 = State SendTARead 1 req
+      | cnt < 4 = State SendTARead (cnt+1) d
+      | otherwise = State (ReadD16 0) 0 d
 
 -- Receive data for Read
-nextStep (State (ReadD16 bv) cnt req) (_, inp) = (nextState, (Nothing, response))
+nextStep (State (ReadD16 bv) cnt d) (_, inp) = (nextState, (Just 0, response))
   where
     -- Receive a bit from the register we're reading
     -- Bit 15 first, bit 0 last!
-    bv' = replaceBit (15-cnt) inp bv
-    response
-      | cnt == 15 = Just $ MDIOReadResult bv'
-      | otherwise = Nothing
-    nextState
-      | cnt < 15 = State (ReadD16 bv') (cnt+1) req
-      -- TODO: Make a new blank `req` here?
-      | otherwise = State Idle 0 req
+    bv' = replaceBit 0 inp $ shiftL bv 1
+    (response, nextState)
+      | cnt >= 15 = (Just $ MDIOReadResult bv', State Idle 0 d)
+      | otherwise = (Nothing, State (ReadD16 bv') (cnt+1) d)
 
 -- Send turn-around field for Write
-nextStep s@(State SendTAWrite _ _) (_, _) = sendTwo WriteD16 (1,0) s
+nextStep (State SendTAWrite cnt d) (_, _) = (nextState, (Just out, Nothing))
+    where
+        (out, nextState)
+            | cnt == 0  = (1, State SendTAWrite 1 d)
+            | otherwise = (0, State WriteD16 0 d)
 
 -- Write data to register
-nextStep (State WriteD16 cnt req) (_, _) = (nextState, (Just out, response))
+nextStep (State WriteD16 _ (_, Nothing)) (_, _) = error "Impossible"
+nextStep (State WriteD16 cnt (sendBv, Just dat)) (_, _) = (nextState, (Just out, response))
   where
-    out = case req of
-      (MDIORead _ _) -> error "Impossible"
-                            -- Get a bit from the register write
-                            -- Bit 15 first, bit 0 last!
-      (MDIOWrite _ _ bv) -> bv ! (15 - cnt)
+    -- Get a bit from the register write
+    -- Bit 15 first, bit 0 last!
+    out = msb dat
+    bv' = shiftL dat 1
     response
       | cnt == 15 = Just $ MDIOWriteAck
       | otherwise = Nothing
     nextState
-      | cnt < 15 = State WriteD16 (cnt+1) req
-      -- TODO: Make a new blank `req` here?
-      | otherwise = State Idle 0 req
+      | cnt < 15 = State WriteD16 (cnt+1) (sendBv, Just bv')
+      | otherwise = State Idle 0 (sendBv, Just bv')
+
 
 mdioComponent
   :: HiddenClockResetEnable dom
