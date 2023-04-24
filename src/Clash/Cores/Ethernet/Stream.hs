22bafeb9a1bcc9c172909dc06e030c6600e6c46b
syberant <sybrand@neuralcoding.com>
2023-04-24 09:29:49 +0200

    Add `mealyToCircuit`, `ifgEnforcer` and `preambleInserter`

--- src/Clash/Cores/Ethernet/Stream.hs
+++ src/Clash/Cores/Ethernet/Stream.hs
@@ -28,6 +28,51 @@ type AxiSingleStreamFwd = Maybe (Axi4StreamM2S ('Axi4StreamConfig 1 0 0) UserTyp
 type AxiConfigTagged = 'Axi4StreamConfig 4 0 16
 
 
+mealyToCircuit :: C.HiddenClockResetEnable dom
+    => NFDataX a
+    => (a -> (Maybe (Axi4StreamM2S conf user), Axi4StreamS2M) -> (a, (Axi4StreamS2M, Maybe (Axi4StreamM2S  conf user))))
+    -> a
+    -> Circuit (Axi4Stream dom conf user) (Axi4Stream dom conf user)
+mealyToCircuit machineAsFunction initialState = Circuit $ circuitFunction where
+  circuitFunction = C.unbundle
+                  . C.mealy machineAsFunction initialState
+                  . C.bundle
+
+preambleInserter :: C.HiddenClockResetEnable dom => Circuit (AxiSingleStream dom) (AxiSingleStream dom)
+preambleInserter = mealyToCircuit machineAsFunction 0 where
+  -- 0: Just passing messages through like normal
+  -- 1: Sending SFD (Start of Frame Delimiter)
+  -- 2-8: Sending normal preamble
+  machineAsFunction :: Index 9 -> (AxiSingleStreamFwd, Axi4StreamS2M) -> (Index 9, (Axi4StreamS2M, AxiSingleStreamFwd))
+  machineAsFunction 0 (inp, recvACK) = (newCounter, (recvACK, inp))
+    where
+      -- If this was the last byte of the frame or not part of a frame,
+      -- then a preamble should be inserted whenever a new frame begins.
+      newCounter = if maybe True _tlast inp then 8 else 0
+  machineAsFunction n (_, recvACK) = (nextStep, (sendACK, Just out))
+            where
+                nextStep = if _tready recvACK then n-1 else n
+                sendACK = Axi4StreamS2M { _tready = False }
+                out = Axi4StreamM2S { _tdata = singleton $ if n == 1 then 0xd5 else 0x55
+                                    , _tkeep = singleton True
+                                    , _tstrb = singleton False
+                                    , _tlast = False
+                                    , _tuser = ()
+                                    , _tid = 0
+                                    , _tdest = 0
+                                    }
+
+
+ifgEnforcer :: C.HiddenClockResetEnable dom => Circuit (AxiSingleStream dom) (AxiSingleStream dom)
+ifgEnforcer = mealyToCircuit machineAsFunction 0 where
+  machineAsFunction :: Index 13 -> (AxiSingleStreamFwd, Axi4StreamS2M) -> (Index 13, (Axi4StreamS2M, AxiSingleStreamFwd))
+  machineAsFunction 0 (inp, recvACK) = (newCounter, (recvACK, inp))
+    where
+      -- If this was the last byte of the frame, then skip 12 cycles for the Inter Frame Gap
+      newCounter = if maybe False _tlast inp then 12 else 0
+  machineAsFunction n (inp, recvACK) = (n-1, (Axi4StreamS2M { _tready = False }, Nothing))
+
+
 streamTestFramePerSecond :: C.HiddenClockResetEnable dom => Circuit () (AxiStream dom)
 streamTestFramePerSecond = Circuit $ circuitFunction where
   circuitFunction ((), recvACK) = ((), out) where
