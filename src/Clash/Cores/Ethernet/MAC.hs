af6c040ba8102551bc51b24e276d5f59da11021c
syberant <sybrand@neuralcoding.com>
2023-05-23 10:55:40 +0200

    Add `bufferFrame` circuit to fix holes

--- src/Clash/Cores/Ethernet/MAC.hs
+++ src/Clash/Cores/Ethernet/MAC.hs
@@ -1,6 +1,6 @@
 module Clash.Cores.Ethernet.MAC ( macCircuits, txMACCircuit, rxMACCircuit ) where
 
-import Clash.Cores.Ethernet.Stream ( SingleByteStream, TaggedStream )
+import Clash.Cores.Ethernet.Stream
 import Clash.Prelude
 import Protocols
 import Protocols.Axi4.Stream
@@ -10,6 +10,7 @@ import Protocols.DfConv ( registerBwd )
 
 -- Components
 import Clash.Cores.Ethernet.CDC ( circuitCDC )
+import Clash.Cores.Ethernet.MAC.BufferFrame
 import Clash.Cores.Ethernet.MAC.ConstructHeader
 import Clash.Cores.Ethernet.MAC.DeconstructHeader
 import Clash.Cores.Ethernet.MAC.FCS
@@ -45,6 +46,14 @@ txMACCircuit :: forall (edom        :: Domain)
   -> Circuit (TaggedStream mdom) (SingleByteStream edom)
 txMACCircuit ethClk ethRst ethEn clk rst en = withEth $ ifgEnforcer
                                                      <| preambleInserter
+
+                                                     -- Put large blockram buffer in the 50 MHz clock domain to meet timing
+                                                     <| downconverter
+                                                     <| circuitCDC clk ethClk rst ethRst en ethEn
+                                                     <| (with50 bufferFrame :: Circuit (FourByteStream mdom) (FourByteStream mdom))
+                                                     <| circuitCDC ethClk clk ethRst rst ethEn en
+                                                     <| upconverter
+
                                                      <| fcsAppender
                                                      <| constructHeader
                                                      <| registerBwd Proxy Proxy
@@ -54,6 +63,7 @@ txMACCircuit ethClk ethRst ethEn clk rst en = withEth $ ifgEnforcer
                                                      <| circuitCDC clk ethClk rst ethRst en ethEn
   where
     withEth = withClockResetEnable ethClk ethRst ethEn
+    with50 = withClockResetEnable clk rst en
 
 rxMACCircuit :: forall (edom        :: Domain)
                        (mdom        :: Domain)
