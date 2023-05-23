854ae2c67f2572a18c41b88e2e5c58432d45db0a
faycel-dev <fayce001@live.com>
2023-05-23 03:30:23 -0400

    Remove preamble

--- src/Clash/Cores/Ethernet/MAC/PreambleRemover.hs
+++ src/Clash/Cores/Ethernet/MAC/PreambleRemover.hs
@@ -6,12 +6,23 @@ import Protocols.Axi4.Stream
 
 import Clash.Cores.Ethernet.Stream ( SingleByteStream, SingleByteStreamFwd, mealyToCircuit )
 
-data State = Idle
+data State = Idle Int | NextState Int
   deriving (Show, Eq, Generic, NFDataX)
 
 preambleRemover :: HiddenClockResetEnable dom => Circuit (SingleByteStream dom) (SingleByteStream dom)
 preambleRemover = mealyToCircuit machineAsFunction initialState where
-  initialState = undefined
+  initialState = Idle 0
   machineAsFunction :: State -> (SingleByteStreamFwd, Axi4StreamS2M) -> (State, (Axi4StreamS2M, SingleByteStreamFwd))
-  -- TODO: Actually implement this:
-  machineAsFunction s (inp, recvACK) = (s, (recvACK, inp))
+  machineAsFunction (Idle n) (inp, recvACK) =  (nextState ,(recvACK , out))
+     where
+        counter 
+          | isNothing inp = 0
+          | _tready recvACK = n+1
+          | otherwise = n
+        nextState
+          | n==8  = NextState (n-8)
+          |otherwise = Idle n
+        out
+          | n < 8 = Nothing
+           -- Discard the preamble bytes
+          | otherwise = inp 
