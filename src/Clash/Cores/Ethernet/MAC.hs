c7f54085f01a81d9fc20bc54e2c065556386fd85
syberant <sybrand@neuralcoding.com>
2023-05-29 12:46:48 +0200

    Add `MAC.hs`

---
+++ src/Clash/Cores/Ethernet/MAC.hs
@@ -0,0 +1,54 @@
+module Clash.Cores.Ethernet.MAC ( macCircuits, txMACCircuit, rxMACCircuit ) where
+
+import Clash.Cores.Ethernet.Stream
+import Clash.Prelude
+import Protocols
+
+-- Components
+import Clash.Cores.Ethernet.CDC ( circuitCDC )
+import Clash.Cores.Ethernet.MAC.ConstructHeader
+import Clash.Cores.Ethernet.MAC.IFG
+import Clash.Cores.Ethernet.Utils ( downconverter, upconverter )
+
+macCircuits :: forall (edom        :: Domain)
+                      (mdom        :: Domain)
+   . (KnownDomain edom, KnownDomain mdom)
+  => Clock edom
+  -> Reset edom
+  -> Enable edom
+  -> Clock mdom
+  -> Reset mdom
+  -> Enable mdom
+  -> ( Circuit (SingleByteStream edom) (TaggedStream mdom)
+     , Circuit (TaggedStream mdom) (SingleByteStream edom)
+     )
+macCircuits ethClk ethRst ethEn clk rst en = (rxMACCircuit ethClk ethRst ethEn clk rst en, txMACCircuit ethClk ethRst ethEn clk rst en)
+
+txMACCircuit :: forall (edom        :: Domain)
+                       (mdom        :: Domain)
+   . (KnownDomain edom, KnownDomain mdom)
+  => Clock edom
+  -> Reset edom
+  -> Enable edom
+  -> Clock mdom
+  -> Reset mdom
+  -> Enable mdom
+  -> Circuit (TaggedStream mdom) (SingleByteStream edom)
+txMACCircuit ethClk ethRst ethEn clk rst en = withEth $ ifgEnforcer
+                                                     <| constructHeader
+                                                     <| downconverter
+                                                     <| circuitCDC clk ethClk rst ethRst en ethEn
+  where
+    withEth = withClockResetEnable ethClk ethRst ethEn
+
+rxMACCircuit :: forall (edom        :: Domain)
+                       (mdom        :: Domain)
+   . (KnownDomain edom, KnownDomain mdom)
+  => Clock edom
+  -> Reset edom
+  -> Enable edom
+  -> Clock mdom
+  -> Reset mdom
+  -> Enable mdom
+  -> Circuit (SingleByteStream edom) (TaggedStream mdom)
+rxMACCircuit _ethClk _ethRst _ethEn _clk _rst _en = undefined
