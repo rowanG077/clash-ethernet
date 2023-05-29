9e0c3e7dcb5dc90db5ab820820f653396b46f136
syberant <sybrand@neuralcoding.com>
2023-05-29 12:53:41 +0200

    Implement `streamTestFramePerSecond`

--- src/Clash/Cores/Ethernet/Frame.hs
+++ src/Clash/Cores/Ethernet/Frame.hs
@@ -1,43 +1,33 @@
-module Clash.Cores.Ethernet.Frame (sendFrameOnPulse, testFrame) where
+{-# language NumericUnderscores #-}
+
+module Clash.Cores.Ethernet.Frame ( testFrame, testPayload, testHeader ) where
 
 import Clash.Prelude
-
-import Data.Maybe (isNothing)
-
-type Byte = BitVector 8
+import Clash.Cores.Ethernet.Stream ( EthernetHeader (..) )
 
 -- | ARP packet captured using Wireshark
 --
 -- To get the CRC as well while capturing use `ethtool -K IFNAME rx-fcs on`.
 -- To receive even packets with a wrong CRC use `ethtool -K IFNAME rx-all on`.
-testFrame :: Vec (8+64) Byte
+testFrame :: Vec (8+64) (Unsigned 8)
 testFrame = preamble ++ (hexToVec 0xffffffffffff54833a354a500806000108000604000154833a354a50c0a80101ffffffffffffc0a801c500000000000000000000000000000000000026544e8b)
 
-hexToVec :: KnownNat n => Unsigned (8*n) -> Vec n Byte
+testHeader :: EthernetHeader
+testHeader = EthernetHeader { destinationMAC = unpack (0xffffffffffff :: BitVector (6*8))
+                            , sourceMAC = unpack (0x54833a354a50 :: BitVector (6*8))
+                            , etherType = 0x0806
+                            }
+
+testPayload :: Vec 28 (Unsigned 8)
+testPayload = hexToVec 0x000108000604000154833a354a50c0a80101ffffffffffffc0a801c5
+
+hexToVec :: KnownNat n => Unsigned (8*n) -> Vec n (Unsigned 8)
 hexToVec x = unpack $ pack x
 
-preamble :: Vec 8 Byte
+preamble :: Vec 8 (Unsigned 8)
 preamble = replicate d7 0x55 :< sfd
     where
         sfd = 0xd5
-
-crcCalculate :: Vec n Byte -> BitVector 32
-crcCalculate _ = 0xBAADF00D
-
--- | Creates a static ethernet frame including preamble and CRC
---
--- NOTE: Needs to have a valid size, add padding to your payload if the size is too small.
--- NOTE:Does not include the inter-frame gap (IFG/IPG)
-createStaticFrame :: forall n . (KnownNat n, (72 <=? 8+6+6+2+n+4) ~ 'True, (8+6+6+2+n+4 <=? 1530) ~ 'True)
-    => Vec 6 Byte
-    -> Vec 6 Byte
-    -> BitVector 16
-    -> Vec n Byte
-    -> Vec (8 + 6 + 6 + 2 + n + 4) Byte
-createStaticFrame src dest eth_type payload = preamble ++ dest ++ src ++ (unpack eth_type) ++ payload ++ crc
-    where
-        crc :: Vec 4 Byte
-        crc = unpack $ crcCalculate $ dest ++ src ++ (unpack eth_type) ++ payload
 
 sendFrameOnPulse :: forall dom . HiddenClockResetEnable dom
     => Signal dom Bool
