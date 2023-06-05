a36121296866e1d86bd8db7441e1796fea4ce3de
Thomas <thomasbroekman2002@gmail.com>
2023-06-05 12:03:35 +0200

    CRC tests

--- tests/unittests.hs
+++ tests/unittests.hs
@@ -3,8 +3,10 @@ import Prelude
 import Test.Tasty
 
 import qualified Test.Cores.Ethernet.Ethernet
+import qualified Test.Cores.Ethernet.CRC
 
 main :: IO ()
 main = defaultMain $ testGroup "."
-  [ Test.Cores.Ethernet.Ethernet.tests
+  [ Test.Cores.Ethernet.Ethernet.tests,
+    Test.Cores.Ethernet.CRC.tests
   ]
